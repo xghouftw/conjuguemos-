835cc392d9fa6649b0043478ae910f1e0779150f
XGHouFTW <68599565+XGHouFTW@users.noreply.github.com>
2020-12-26 12:09:06 -0500

    Add files via upload

---
+++ conjuguemos.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class conjuguemos {
+	
+static String Pronouns[] = {"Yo","Tú","Él, Ella, Ustedes","Nosotros","Vosotros","Ellos, Ellas, Ustedes"};
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Welcome to the Spanish verb conjugation trainer!\nType a number from the menu to continue.\n");
+		String Menu[] = {"Quit","Present Indicative","Preterite","Imperfect"};
+		
+		String next;
+		do {
+			for (int i = 0; i < Menu.length; i++) System.out.println(i + ". " + Menu[i]);
+			next = input.nextLine();
+			if (next.equals("1")) PresentIndicative();
+			if (next.equals("2")) Preterite();
+			if (next.equals("3")) Imperfect();
+			
+		} while (!next.equals("0"));
+		
+		
+	}
+	
+	public static void PresentIndicative() {
+			
+		
+	}
+	public static void Preterite() {
+		
+		
+		
+	}
+	public static void Imperfect() {
+		
+		
+		
+	}
+
+}

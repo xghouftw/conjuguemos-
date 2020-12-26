1e9bf79706cbe24b5efa4620d163fcc502d2b985
XGHouFTW <68599565+XGHouFTW@users.noreply.github.com>
2020-12-26 12:54:51 -0500

    Update conjuguemos.java

--- conjuguemos.java
+++ conjuguemos.java
@@ -4,9 +4,9 @@ public class conjuguemos {
 	
 static String Pronouns[] = {"Yo","Tú","Él, Ella, Ustedes","Nosotros","Vosotros","Ellos, Ellas, Ustedes"};
 static ArrayList<String> Verbs = new ArrayList<String>();
+static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) throws IOException {
-		
-		Scanner input = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new FileReader("VerbBank.txt"));
 		String verb = "";
 		while ((verb = br.readLine()) != null) Verbs.add(verb);
@@ -38,11 +38,37 @@ static ArrayList<String> Verbs = new ArrayList<String>();
 		
 	}
 	public static void Imperfect() {
-		System.out.println("---Imperfect Mode---\nRespond 0 to Quit");
-		
+		System.out.println("---Imperfect Mode---\nRespond 0 to Quit to the main menu");
+
 		while (true) {
+			String question = Verbs.get((int)(Math.random()*Verbs.size()));
+			int pron = (int)(Math.random()*6);
 			
+			System.out.println(Pronouns[pron] + " (" + question + ")");
+			String response = input.nextLine();
+			if (response.equals("0")) return;
 			
+			String correct = question;
+			correct = question.substring(0,question.length()-2);
+			String ending = question.substring(question.length()-2);
+			if (ending.equals("ar")) {
+				if (pron == 0) correct += "aba";
+				if (pron == 1) correct += "abas";
+				if (pron == 2) correct += "aba";
+				if (pron == 3) correct += "ábamos";
+				if (pron == 4) correct += "abais";
+				if (pron == 5) correct += "aban";
+			}
+			if (ending.equals("er") || ending.equals("ir")) {
+				if (pron == 0) correct += "ía";
+				if (pron == 1) correct += "ías";
+				if (pron == 2) correct += "ía";
+				if (pron == 3) correct += "íamos";
+				if (pron == 4) correct += "íais";
+				if (pron == 5) correct += "ían";
+			}
+			if (response.equals(correct)) System.out.println("Correct!");
+			else System.out.println("Incorrect!");
 		}
 		
 	}

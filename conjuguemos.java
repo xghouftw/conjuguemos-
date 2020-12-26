6a0298bf5a066da7d5bb2a7e0cf3d9dadc8ca89e
XGHouFTW <68599565+XGHouFTW@users.noreply.github.com>
2020-12-26 12:23:35 -0500

    Update conjuguemos.java

--- conjuguemos.java
+++ conjuguemos.java
@@ -1,11 +1,17 @@
 import java.util.*;
+import java.io.*;
 public class conjuguemos {
 	
 static String Pronouns[] = {"Yo","Tú","Él, Ella, Ustedes","Nosotros","Vosotros","Ellos, Ellas, Ustedes"};
-
-	public static void main(String[] args) {
+static ArrayList<String> Verbs = new ArrayList<String>();
+	public static void main(String[] args) throws IOException {
 		
 		Scanner input = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new FileReader("Verbs.txt"));
+		String verb = "";
+		while ((verb = br.readLine()) != null) Verbs.add(verb);
+		
+		
 		System.out.print("***Welcome to the Spanish verb conjugation trainer!***\nType a number from the menu to continue.\n");
 		String Menu[] = {"Quit","Present Indicative","Preterite","Imperfect"};
 		
@@ -19,7 +25,7 @@ static String Pronouns[] = {"Yo","Tú","Él, Ella, Ustedes","Nosotros","Vosotros
 			
 		} while (!next.equals("0"));
 		System.out.println("Thank you for playing Spanish verb conjugation trainer. Practice makes Permanent!");
-		
+
 	}
 	
 	public static void PresentIndicative() {

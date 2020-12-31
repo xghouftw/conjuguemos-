c7ec09377923ad75879a57ab25a90ea7a0c8fdaf
XGHouFTW <68599565+XGHouFTW@users.noreply.github.com>
2020-12-30 21:41:53 -0500

    Update conjuguemos.java

--- conjuguemos.java
+++ conjuguemos.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class conjuguemos {
 	
-static String Pronouns[] = {"Yo","Tú","Él, Ella, Usted","Nosotros","Vosotros","Ellos, Ellas, Ustedes"};
+static String Pronouns[] = {"Yo","Tú","Él, Ella, Ustedes","Nosotros","Vosotros","Ellos, Ellas, Ustedes"};
 static ArrayList<String> Verbs = new ArrayList<String>();
 static Scanner input = new Scanner(System.in);
 
@@ -11,7 +11,6 @@ static Scanner input = new Scanner(System.in);
 		String verb = "";
 		while ((verb = br.readLine()) != null) Verbs.add(verb);
 		
-		
 		System.out.print("***Welcome to the Spanish verb conjugation trainer!***\nType a number from the menu to continue.\n");
 		String Menu[] = {"Quit","Present Indicative","Preterite","Imperfect"};
 		
@@ -19,9 +18,26 @@ static Scanner input = new Scanner(System.in);
 		do {
 			for (int i = 0; i < Menu.length; i++) System.out.println(i + ". " + Menu[i]);
 			next = input.nextLine();
-			if (next.equals("1")) PresentIndicative();
-			if (next.equals("2")) Preterite();
-			if (next.equals("3")) Imperfect();
+			if (next.equals("1")) {
+				
+				PresentIndicative();
+			}
+			if (next.equals("2")) {
+				Preterite();
+			}
+			if (next.equals("3")) {
+				System.out.println("---Imperfect Mode--\nRespond 0 to Quit to main menu");
+				String response;
+				do {
+					String question = Verbs.get((int)(Math.random()*Verbs.size()));
+					int subject = (int)(Math.random()*6);
+					System.out.println(Pronouns[subject] + " (" + question + ")");
+					response = input.nextLine();
+					String answer = Imperfect(subject, question);
+					if (response.equals(answer)) System.out.println("Correct!");
+					else System.out.println("Incorrect! The correct conjugation is: " + answer + ".");
+				} while(!response.equals("0"));
+			}
 			
 		} while (!next.equals("0"));
 		System.out.println("Thank you for playing Spanish verb conjugation trainer. Practice makes Permanent!");
@@ -29,48 +45,35 @@ static Scanner input = new Scanner(System.in);
 	}
 	
 	public static void PresentIndicative() {
-			
-		
+
 	}
 	public static void Preterite() {
 		
-		
-		
 	}
-	public static void Imperfect() {
-		System.out.println("---Imperfect Mode---\nRespond 0 to Quit to the main menu");
-
-		while (true) {
-			String question = Verbs.get((int)(Math.random()*Verbs.size()));
-			int pron = (int)(Math.random()*6);
-			
-			System.out.println(Pronouns[pron] + " (" + question + ")");
-			String response = input.nextLine();
-			if (response.equals("0")) return;
-			
-			String correct = question;
-			correct = question.substring(0,question.length()-2);
+	
+	public static String Imperfect(int subject, String question) {
+			String correct = "";
 			String ending = question.substring(question.length()-2);
 			
-			if (ending.equals("ar")) {
-				String add[] = {"aba","abas","aba","ábamos","abais","aban"};
-				correct += add[pron];
-			}
-			if (ending.equals("er") || ending.equals("ir")) {
-				String add[] = {"ía","ías","ía","íamos","íais","ían"};
-				correct += add[pron];
-			}
-			
 			String irregulars[][] = {{"ser","era","eras","era","éramos","erais","eran"},{"ir","iba","ibas","iba","íbamos","ibais","iban"},{"ver","veía","veías","veía","veíamos","veíais","veían"}};
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < irregulars.length; i++) {
 				if (question.equals(irregulars[i][0])) {
-					correct = irregulars[i][pron+1];
+					correct = irregulars[i][subject+1];
+					return correct;
 				}
 			}
 			
-			if (response.equals(correct)) System.out.println("Correct!");
-			else System.out.println("Incorrect!");
+			correct = question.substring(0, question.length()-2);
+			if (ending.equals("ar")) {
+				String add[] = {"aba","abas","aba","ábamos","abais","aban"};
+				correct += add[subject];
+				return correct;
+			}
+			if (ending.equals("er") || ending.equals("ir")) {
+				String add[] = {"ía","ías","ía","íamos","íais","ían"};
+				correct += add[subject];
+				return correct;
+			}
+			return "Error";
 		}
 	}
-
-}
